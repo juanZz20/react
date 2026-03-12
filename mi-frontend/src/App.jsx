9d332bcf1230d6f16d4c760a592ebf567aef5af6
Juan <mar82697@gmail.com>
2026-03-12 00:02:13 -0500

    creacion de metodo get y post

--- mi-frontend/src/App.jsx
+++ mi-frontend/src/App.jsx
@@ -1,28 +1,60 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, use } from 'react'
 
 function App() {
   // info tiene los datos actuales (cargando) y el set info carga los datos del servidor
   const [info, setInfo] = useState({ mensaje: "Cargando...", usuario: "" });
+  const[tareas,setTareas]=useState([])
+  const [texto, setTexto] = useState("")
+
+  const obtenertareas=()=> {
+    fetch("http://localhost:5003/api/tareas")
+    .then(res =>res.json())
+    .then(data =>  setTareas(data));
+  }
 
   useEffect(() => {
-    // IMPORTANTE: Asegúrate de que este puerto sea el de tu API de C#
-    // Lo encuentras en launchSettings.json de tu proyecto ASP.NET
-    fetch("http://localhost:5003/api/saludo") 
-      //el servidor envia un formato json para que javascript lo lea
-      .then(res => res.json())
-      //enviamos la informacion al setinfo para que se cargue
-      .then(data => setInfo(data))
-      .catch(err => console.error("API apagada o error de CORS:", err));
+    obtenertareas();
       //los corchetes le dicen a react que solo cargue esta info una vez 
   }, []);
+
+  const guardarTarea = async () =>{
+
+    if (texto === ""){
+      return;
+    }
+    await fetch("http://localhost:5003/api/tareas",{
+    method:"POST",
+    headers: {"content-type": "application/json" },
+    body:JSON.stringify({nombre:texto})
+    })
+    setTexto("");
+    obtenertareas();
+  }
   //esto es lo que el usuario ve 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>React + ASP.NET Core</h1>
-      <div style={{ border: '1px solid white', padding: '20px', borderRadius: '10px' }}>
-        <p>Mensaje desde C#: <strong>{info.mensaje}</strong></p>
-        <p>Programador: <strong>{info.usuario}</strong></p>
+    <div style={{ padding: '40px', color: 'white'}}>
+      <h1>📝 Mis Tareas Pendientes</h1>
+
+      <div style={{ marginBottom: '20px' }}>
+        <input
+          type="text"
+          value={texto}
+          onChange={(e) => setTexto(e.target.value)}
+          placeholder="Escribe una tarea..."
+          style={{ padding: '10px', borderRadius: '5px', border: 'none' }}
+        />
+        <button onClick={guardarTarea} style={{ marginLeft: '10px', padding: '10px' }}>
+          Agregar Tarea
+        </button>
       </div>
+
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {tareas.map(t => (
+          <li key={t.id} style={{ background: '#444', margin: '10px 0', padding: '15px', borderRadius: '8px' }}>
+            ✅ {t.nombre}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
